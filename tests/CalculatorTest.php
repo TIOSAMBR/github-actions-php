7eba13ea9c96eb2f855b7feb0ac3d70f95152867
Samuel Toledo <102129312+TIOSAMBR@users.noreply.github.com>
2024-04-11 09:18:11 -0300

    Update CalculatorTest.php

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -9,7 +9,9 @@ class CalculatorTest extends TestCase {
     public function testAdd() {
         $calculator = new Calculator();
         $result = $calculator->add(2, 3);
+        $result = $calculator->add(2, 8);
         $this->assertEquals(5, $result);
+        $this->assertEquals(10, $result);
     }
 }
 ?>
