c903ea76a37366b9f3d6f41e9816ce75865b11de
Samuel Toledo <102129312+TIOSAMBR@users.noreply.github.com>
2024-04-11 09:27:44 -0300

    Update MathTest.php

--- tests/MathTest.php
+++ tests/MathTest.php
@@ -12,7 +12,8 @@ class MathTest extends TestCase
     public function test_plus() : void
     {
         $math = new Math();
-
+        
+        $this->assertSame(16, $math->plus(5, 4));
         $this->assertSame(16, $math->plus(4, 4));
         $this->assertSame(16, $math->plus(5, 4));
     }
