642f56befd0856811959d9fb65dd0131f58e6881
Samuel Toledo <102129312+TIOSAMBR@users.noreply.github.com>
2024-04-11 09:44:17 -0300

    Update CalculatorTest.php

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -13,7 +13,7 @@ use PHPUnit\Framework\TestCase;
 class CalculatorTest extends TestCase {
     public function testAdd() {
         $calculator = new Calculator();
-        $result = $calculator->add(2, 3);
+        $result = $calculator->add(2, 5);
         $result = $calculator->add(2, 8);
         $result = $calculator->add(2, 10);
         $this->assertEquals(4, $result);
